04fdf06abcfa66b4b090f23aaae5430234a74d94
AJN <buzz781@gmail.com>
2024-11-26 05:14:18 +0900

    feat: 코드포맷팅 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src'],
+          ['@', './'],
+        ],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+    },
+  },
 };
