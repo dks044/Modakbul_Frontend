8cab0416e19aa59600b7145913da634556e56184
AJN <buzz781@gmail.com>
2024-11-26 05:11:24 +0900

    feat: 스택 네비게이션 및 reanimated 추가

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,16 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
 
 /**
  * Metro configuration
- * https://reactnative.dev/docs/metro
  *
  * @type {import('metro-config').MetroConfig}
  */
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// 기본 설정을 가져오고, reanimated 설정으로 감싸기
+module.exports = wrapWithReanimatedMetroConfig(
+  mergeConfig(getDefaultConfig(__dirname), config)
+);
